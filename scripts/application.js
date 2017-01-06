2dbaa9c2c93609befa6f3b42bfa310c32cd2bdbd
ToTenMilan <grzegorz.od.tych.milanowskich@gmail.com>
2017-01-06 11:40:57 +0000

    Debug the reset button

--- scripts/application.js
+++ scripts/application.js
@@ -1,40 +1,38 @@
 /* global $ */
 
 var boardSize = 20;
-var squareSize = 400/boardSize;
+
 $(document).ready(function() {
     
-    // if (($('table').length) === 0) {
-        printBoard(boardSize);
-    // }
+    printBoard(boardSize);
     
-    $('.square').on('mouseenter', function() {
+    $('#container').on('mouseenter', '.square', function() {
     	$(this).css('background-color', 'blue');
     });
-    $('.square').on('click', function() {
-    	$(this).css('background-color', 'red');
-    });
-    // $('#button').click(function() {
-    //     var grid = parseInt(prompt("How dense should be your new awesome board? Please type a number between 1 and 64:"));
-    //     $('.square').remove();
-    //     boardSize = grid;
-    //     printBoard(boardSize);
+    // $('.square').on('click', function() {
+    // 	$(this).css('background-color', 'red');
     // });
-    
+    $('#button').click(function() {
+        var grid = parseInt(prompt("How dense should be your new awesome board? Please type a number between 1 and 64:"));
+        $('.square').remove();
+        boardSize = grid;
+        printBoard(boardSize);
+    });
 });
 
+// part of below function from here http://stackoverflow.com/questions/25999747/how-to-create-board-nxn-using-javascript-and-jquery/25999892
 function printBoard(i_BoardSize) {
     var maxRow = parseInt(i_BoardSize);
     var maxCol = parseInt(i_BoardSize);
-    // var num = 1;
 
     var myTable = $('<div id="board"></div>').appendTo("#container");
     for (var row = maxRow - 1; row >= 0; row--) {
         var myRow = $('<div class="row"></tr>').appendTo(myTable);
         for (var col = 0; col < maxCol; col++) {
             myRow.append('<div class="square"></td>');
-            $('.square').css({'width': squareSize, 'height': squareSize});
-            $('.row').css('height', squareSize);
+            var squareSize = 400/boardSize;
+            $('.square').css({'width': squareSize + 'px', 'height': squareSize + 'px'});
+            $('.row').css('height', squareSize + 'px');
         }
     }
 }

8d99720f0f021db8c7532d3b828dbd7089238857
ToTenMilan <grzegorz.od.tych.milanowskich@gmail.com>
2017-01-06 13:37:41 +0000

    Add color buttons and fix reset prompt

--- scripts/application.js
+++ scripts/application.js
@@ -5,23 +5,50 @@ var boardSize = 20;
 $(document).ready(function() {
     
     printBoard(boardSize);
+    var backgroundColor = 'black';
     
     $('#container').on('mouseenter', '.square', function() {
         
-    	$(this).css({'background-color': 'red'});
+    	$(this).css({'background-color': backgroundColor});
     	var opacity = +$(this).css('opacity');
     	if (opacity < 1) {
     	    opacity += 0.1;
     	    $(this).css('opacity', opacity);
     	}
     });
+    
+    $('#red').click(function() {
+       backgroundColor = 'red';
+       $('.square').css('background-color', backgroundColor); 
+    });
+    $('#green').click(function() {
+       backgroundColor = 'green';
+       $('.square').css('background-color', backgroundColor); 
+    });
+    $('#blue').click(function() {
+       backgroundColor = 'blue';
+       $('.square').css('background-color', backgroundColor); 
+    });
+    $('#yellow').click(function() {
+       backgroundColor = 'yellow';
+       $('.square').css('background-color', backgroundColor); 
+    });
+    $('#black').click(function() {
+       backgroundColor = 'black';
+       $('.square').css('background-color', backgroundColor); 
+    });
     // $('.square').on('click', function() {
     // 	$(this).css('background-color', 'red');
     // });
     $('#button').click(function() {
         var grid = parseInt(prompt("How dense should be your new awesome board? Please type a number between 1 and 64:"));
+        if (grid > 64) {
+            alert("Chill man that's too big. We don't want to hang your machine. Your new board has been set to max of 64x64 squares");
+            boardSize = 64;
+        } else {
+            boardSize = grid;
+        }
         $('.square').remove();
-        boardSize = grid;
         printBoard(boardSize);
     });
 });
